a7e295814e30b2000d6e4340d77e75244a270ce4
Lera <128050534+MsMiracle18@users.noreply.github.com>
2023-09-09 13:28:51 +0200

    Update import mongoengine.py

--- import mongoengine.py
+++ import mongoengine.py
@@ -20,6 +20,20 @@ class Quote(db.Document):
     def __str__(self):
         return self.text
 
+# Завантаження даних авторів та цитат з файлів (припустимо, що ми маємо файли authors_data та quotes_data)
+for author_data in authors_data:
+    author = Author.objects(name=author_data['name']).first()
+    if not author:
+        author = Author(**author_data)
+        author.save()
+
+for quote_data in quotes_data:
+    author = Author.objects(name=quote_data['author']).first()
+    if author:
+        quote_data['author'] = author
+        quote = Quote(**quote_data)
+        quote.save()
+
 # Створення нового автора
 author = Author(name='John Doe', birth_year=1980, nationality='American')
 author.save()
